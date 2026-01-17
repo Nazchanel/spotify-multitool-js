bb0854a003f7abd5a93a755e5f097345f9451d9a
Eshan Iyer <iyereshan@gmail.com>
2026-01-17 16:30:38 -0600

    init
    Indentation

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spotify Multitool
+Easy way to do things with the API that can't be done from the app like true random shuffle.
+
+1. True Shuffle
+2. Save a Playlist to directory (download Files) [TO BE IMPLEMENTED]
+    - Update that directory checking the music files on command
---
+++ server.js
@@ -0,0 +1,233 @@
+require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+const SpotifyWebApi = require('spotify-web-api-node');
+const path = require('path');
+
+const app = express();
+const PORT = 8000;
+
+/* -------------------------
+   Environment
+------------------------- */
+
+const {
+  CLIENT_ID,
+  SECRET_CLIENT_ID,
+  REDIRECT_URL,
+  SESSION_SECRET
+} = process.env;
+
+if (!CLIENT_ID || !SECRET_CLIENT_ID || !REDIRECT_URL) {
+  throw new Error('Missing Spotify credentials in .env');
+}
+
+const SCOPE = [
+  'user-library-read',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'streaming'
+].join(' ');
+
+/* -------------------------
+   Middleware
+------------------------- */
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+  secret: SESSION_SECRET || 'SUPER_SECRET_KEY',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+/* -------------------------
+   Helpers
+------------------------- */
+
+function createSpotifyClient() {
+  return new SpotifyWebApi({
+    clientId: CLIENT_ID,
+    clientSecret: SECRET_CLIENT_ID,
+    redirectUri: REDIRECT_URL
+  });
+}
+
+function getSpotifyFromSession(req) {
+  if (!req.session.tokenInfo) return null;
+
+  const sp = createSpotifyClient();
+  sp.setAccessToken(req.session.tokenInfo.access_token);
+  sp.setRefreshToken(req.session.tokenInfo.refresh_token);
+  return sp;
+}
+
+async function getUserPlaylists(sp) {
+  let playlists = [];
+  let data = await sp.getUserPlaylists({ limit: 50 });
+
+  playlists.push(...data.body.items);
+
+  while (data.body.next) {
+    data = await sp.getUserPlaylists({
+      limit: 50,
+      offset: playlists.length
+    });
+    playlists.push(...data.body.items);
+  }
+  return playlists;
+}
+
+async function getPlaylistTracks(sp, playlistId) {
+  let tracks = [];
+  let offset = 0;
+
+  while (true) {
+    const data = await sp.getPlaylistTracks(playlistId, {
+      limit: 100,
+      offset
+    });
+
+    for (const item of data.body.items) {
+      if (!item.track || item.track.is_local) continue;
+      tracks.push(item.track);
+    }
+
+    if (!data.body.next) break;
+    offset += 100;
+  }
+  return tracks;
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+/* -------------------------
+   Routes
+------------------------- */
+
+app.get('/', (req, res) => {
+  if (!req.session.tokenInfo) {
+    return res.redirect('/login');
+  }
+  return res.redirect('/choose');
+});
+
+app.get('/login', (req, res) => {
+  const sp = createSpotifyClient();
+  const authUrl = sp.createAuthorizeURL(SCOPE.split(' '));
+  res.redirect(authUrl);
+});
+
+app.get('/callback', async (req, res) => {
+  const code = req.query.code;
+  if (!code) return res.send('Authorization failed');
+
+  const sp = createSpotifyClient();
+
+  try {
+    const data = await sp.authorizationCodeGrant(code);
+    req.session.tokenInfo = data.body;
+    res.redirect('/choose');
+  } catch (err) {
+    res.send(`Error getting access token: ${err.message}`);
+  }
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => res.redirect('/'));
+});
+
+app.get('/choose', async (req, res) => {
+  const sp = getSpotifyFromSession(req);
+  if (!sp) return res.redirect('/login');
+
+  try {
+    const playlists = await getUserPlaylists(sp);
+    res.render('choose_playlist', { playlists });
+  } catch (err) {
+    res.send('Error fetching playlists');
+  }
+});
+
+app.post('/shuffle', async (req, res) => {
+  const sp = getSpotifyFromSession(req);
+  if (!sp) return res.redirect('/login');
+
+  const playlistId = req.body.playlist_id;
+  if (!playlistId) {
+    return res.render('shuffle', {
+      message: 'No playlist selected.',
+      playlist_name: null,
+      playlist_image: null,
+      tracks: []
+    });
+  }
+
+  try {
+    const tracks = await getPlaylistTracks(sp, playlistId);
+    if (!tracks.length) {
+      return res.render('shuffle', {
+        message: 'No tracks found in this playlist.',
+        playlist_name: null,
+        playlist_image: null,
+        tracks: []
+      });
+    }
+
+    shuffleArray(tracks);
+    const uris = tracks.map(t => t.uri);
+
+    await sp.play({ uris });
+
+    const playlistData = await sp.getPlaylist(playlistId);
+
+    res.render('shuffle', {
+      message: 'Shuffling and playback started!',
+      playlist_name: playlistData.body.name,
+      playlist_image: playlistData.body.images[0]?.url || null,
+      tracks
+    });
+
+  } catch (err) {
+    res.render('shuffle', {
+      message:
+        'No active Spotify device found. Please open Spotify on one of your devices and try again.',
+      playlist_name: null,
+      playlist_image: null,
+      tracks: []
+    });
+  }
+});
+
+
+app.get('/playlist/:id', async (req, res) => {
+  const sp = getSpotifyFromSession(req);
+  if (!sp) return res.redirect('/login');
+
+  try {
+    const tracks = await getPlaylistTracks(sp, req.params.id);
+    res.render('playlist', { tracks });
+  } catch (err) {
+    res.send('Error loading playlist');
+  }
+});
+
+/* -------------------------
+   Start server
+------------------------- */
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
